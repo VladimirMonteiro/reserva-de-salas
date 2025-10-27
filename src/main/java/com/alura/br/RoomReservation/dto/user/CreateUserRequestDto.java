c9e4aac2a10c5f7d30f9c4095427e4cc5b264271
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-10-27 11:33:48 -0300

    FIx: Resolve validations in feat create user

--- src/main/java/com/alura/br/RoomReservation/dto/user/CreateUserRequestDto.java
+++ src/main/java/com/alura/br/RoomReservation/dto/user/CreateUserRequestDto.java
@@ -7,11 +7,9 @@ import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.br.CPF;
 
 public record CreateUserRequestDto(@NotBlank(message = "O nome é obrigatório.") String name,
-                                   @NotBlank(message = "O CPF é obrigatório.") @CPF String cpf,
-                                   @NotNull(message = "A idade é obrigatório.") Integer age,
-                                   @NotBlank(message = "O e-mail é obrigatório,") @Email String email,
+                @NotBlank(message = "O CPF é obrigatório.") @CPF(message = "O CPF informado é inválido.") String cpf,
+                @NotNull(message = "A idade é obrigatório.") Integer age,
+                @NotBlank(message = "O e-mail é obrigatório,") @Email(message = "Formato do e-mail incorreto,") String email,
 
-                                   @NotBlank(message = "O telefone é obrigatório.")
-                      @Pattern(regexp = "\\(\\d{2}\\) \\d{4,5}-\\d{4}",
-                              message = "Telefone deve seguir o padrão (XX) XXXXX-XXXX") String phone) {
+                @NotBlank(message = "O telefone é obrigatório.") @Pattern(regexp = "\\(\\d{2}\\) \\d{4,5}-\\d{4}", message = "Telefone deve seguir o padrão (XX) XXXXX-XXXX") String phone) {
 }
--- src/main/java/com/alura/br/RoomReservation/services/implementations/UserService.java
+++ src/main/java/com/alura/br/RoomReservation/services/implementations/UserService.java
@@ -6,6 +6,8 @@ import com.alura.br.RoomReservation.repositories.UserRepository;
 import com.alura.br.RoomReservation.services.IUserService;
 import com.alura.br.RoomReservation.services.exceptions.UserAlreadyExistsException;
 import com.alura.br.RoomReservation.utils.UserMapper;
+
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,20 +15,29 @@ public class UserService implements IUserService {
 
     private final UserRepository userRepository;
 
-    public UserService (UserRepository userRepository) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
-    public UserDto createUser (CreateUserRequestDto dto) {
-        userRepository.findByCpf(dto.cpf())
-                .ifPresent(u -> {
-                    throw new UserAlreadyExistsException("Usuário já cadastrado.");
-                });
-
+    public UserDto createUser(CreateUserRequestDto dto) {
         var newUser = UserMapper.toEntity(dto);
-        var userSaved = UserMapper.toDto(newUser);
-        userRepository.save(newUser);
-        return userSaved;
+
+        try {
+            var savedUser = userRepository.save(newUser);
+            return UserMapper.toDto(savedUser);
+        } catch (DataIntegrityViolationException e) {
+            String msg = e.getMostSpecificCause().getMessage();
+
+            if (msg.contains("users_cpf_key")) {
+                throw new UserAlreadyExistsException("CPF já cadastrado.");
+            } else if (msg.contains("users_email_key")) {
+                throw new UserAlreadyExistsException("Email já cadastrado.");
+            } else if (msg.contains("users_phone_key")) {
+                throw new UserAlreadyExistsException("Telefone já cadastrado.");
+            } else {
+                throw e;
+            }
+        }
     }
 }
