475eacdd5f2e73b2945f1dbd02fc4f4acfa697f8
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-10-28 20:57:29 -0300

    Refactor: removed imports not useds

--- src/test/java/com/alura/br/RoomReservation/strategy/userValidations/CpfAlreadyExistsValidationTest.java
+++ src/test/java/com/alura/br/RoomReservation/strategy/userValidations/CpfAlreadyExistsValidationTest.java
@@ -8,12 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
