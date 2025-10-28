4ee662b15bc96f64069f21eb4f8ce8c1a2e48a4d
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-10-28 11:07:22 -0300

    Fix: Resolve order of the atributes

--- src/main/java/com/alura/br/RoomReservation/dto/user/UserDto.java
+++ src/main/java/com/alura/br/RoomReservation/dto/user/UserDto.java
@@ -1,4 +1,4 @@
 package com.alura.br.RoomReservation.dto.user;
 
-public record UserDto(String name, String cpf, Integer age, String email, String phone) {
+public record UserDto(String name, String cpf, Integer age, String phone, String email) {
 }
