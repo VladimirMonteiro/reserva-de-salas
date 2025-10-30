8caf8159d0135a142fac059e038d66a8d244aab9
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-10-30 11:14:57 -0300

    Test: find all rooms with pagination

--- src/test/java/com/alura/br/RoomReservation/services/implementations/RoomServiceTest.java
+++ src/test/java/com/alura/br/RoomReservation/services/implementations/RoomServiceTest.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -18,6 +19,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import com.alura.br.RoomReservation.dto.room.CreateRoomRequestDto;
 import com.alura.br.RoomReservation.dto.room.RoomDto;
@@ -86,6 +90,25 @@ class RoomServiceTest {
         var exception = assertThrows(ObjectNotFoundException.class, () -> roomService.findById(anyLong()));
 
         assertEquals("Sala não encontrada.", exception.getMessage());
+    }
+
+    @Test
+    void shouldReturnListOfRooms() {
+        var listRoom = List.of(room);
+        Page<Room> roomsPage = new PageImpl<>(listRoom);
+
+        when(roomRepository.findAll(any(Pageable.class))).thenReturn(roomsPage);
+
+        var result = roomService.findAll(0, 20);
+
+         assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals(room.getName(), result.getFirst().name());
+        assertEquals(room.getCapacity(), result.getFirst().capacity());
+        assertEquals(room.getRoomStatus(), result.getFirst().roomStatus());
+        assertEquals(room.getReservations().size(), result.get(0).reservations().size());
+      
+        verify(roomRepository, times(1)).findAll(any(Pageable.class));
     }
 
 
