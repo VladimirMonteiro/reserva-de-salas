fd498e23faeffd2ad4fa5226d1019b700f4a39ea
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-11-07 13:47:48 -0300

    Test: find reservation by id

---
+++ src/test/java/com/alura/br/RoomReservation/services/implementations/ReservationServiceTest.java
@@ -0,0 +1,115 @@
+package com.alura.br.RoomReservation.services.implementations;
+
+import com.alura.br.RoomReservation.dto.reservation.ReservationDto;
+import com.alura.br.RoomReservation.dto.room.RoomSummaryDto;
+import com.alura.br.RoomReservation.dto.user.UserDto;
+import com.alura.br.RoomReservation.models.Reservation;
+import com.alura.br.RoomReservation.models.Room;
+import com.alura.br.RoomReservation.models.User;
+import com.alura.br.RoomReservation.models.enums.ReservationStatus;
+import com.alura.br.RoomReservation.models.enums.RoomStatus;
+import com.alura.br.RoomReservation.repositories.ReservationRepository;
+import com.alura.br.RoomReservation.repositories.RoomRepository;
+import com.alura.br.RoomReservation.repositories.UserRepository;
+import com.alura.br.RoomReservation.services.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ReservationServiceTest {
+
+    @InjectMocks
+    private ReservationService reservationService;
+
+    @Mock
+    private ReservationRepository reservationRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private RoomRepository roomRepository;
+
+    @Mock
+    private Reservation reservation;
+
+    private UserDto userDto;
+    private ReservationDto reservationDto;
+    private RoomSummaryDto roomDto;
+
+    @BeforeEach
+    void setUp () {
+
+        var user = new User(1L, "Ana", "253.407.490-37", 30, "(51) 99999-9999", "ana@email.com", null);
+        var room = new Room(1L, "Sala reunião", 30, RoomStatus.ACTIVE, null);
+
+        reservation = new Reservation(1L,
+                LocalDate.of(2025, 11, 6),
+                LocalDate.of(2025, 11, 7),
+                ReservationStatus.ACTIVE,
+                room,
+                user);
+        userDto = buildUserDto();
+        roomDto = buildRoomDto();
+        reservationDto = buildReservationDto();
+    }
+
+
+    @Test
+    void createReservation () {
+    }
+
+    @Test
+    void shouldReturnReservationDtoWhenIdIsValid () {
+        when(reservationRepository.findById(anyLong())).thenReturn(Optional.of(reservation));
+
+        var result = reservationService.findById(1L);
+
+        assertNotNull(result);
+        assertEquals(1L, result.id());
+        assertEquals("Ana", result.userDto().name());
+        assertEquals("Sala reunião", result.roomDto().name());
+    }
+
+    @Test
+    void shouldThrowObjectNotFoundWhenReservationNotFound () {
+        when(reservationRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        var exception = assertThrows(ObjectNotFoundException.class, () -> reservationService.findById(1L));
+
+        assertEquals("Reserva não encontrada.", exception.getMessage());
+    }
+
+
+    private ReservationDto buildReservationDto () {
+        return new ReservationDto(1L,
+                LocalDate.of(2025, 11, 6),
+                LocalDate.of(2025, 11, 7),
+                ReservationStatus.ACTIVE,
+                userDto,
+                roomDto);
+    }
+
+    private UserDto buildUserDto () {
+        return new UserDto(1L, "Ana",
+                "253.407.490-37",
+                30,
+                "(51) 99999-9999",
+                "ana@email.com");
+    }
+
+    private RoomSummaryDto buildRoomDto () {
+        return new RoomSummaryDto(1L, "Sala reunião", 30, RoomStatus.ACTIVE);
+    }
+}
